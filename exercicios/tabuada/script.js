d1c1174ebee8f6df4f18eccea9d25d7c54edce41
unknown <abreuroberto2015@gmail.com>
2021-07-18 21:07:57 -0300

    fim

--- exercicios/tabuada/script.js
+++ exercicios/tabuada/script.js
@@ -1,18 +1,23 @@
-function tabuada(){
-    let num = document.getElementById('txtn')
-    let tab = document.getElementById('seltab')
+var tab = document.getElementById('seltab')
 
-    if (num.Value.length === 0){
-        window.alert('Por favor, digite um número!')
-    }else{
+function tabuada() {
+    let num = document.getElementById('txtn')
+    if (num.value.length == 0) {
+        alert('Digite um número')
+    } else {
         let n = Number(num.value)
         let c = 1
-        while (c <= 10){
+        tab.innerHTML = ''
+        while(c <=10) {
             let item = document.createElement('option')
-            item.text = `${n} x $(c) = ${n*c}`
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}` // Importante para o PHP
             tab.appendChild(item)
             c++
-
         }
     }
+}
+
+function limpar() {
+    tab.innerHTML = '<option>Digite outro valor</option>'
 }
